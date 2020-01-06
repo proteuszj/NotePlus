ba7ee54338cac6c1d6962f54c743417763b6760d
ZhangJun <proteuszj@163.com>
2020-01-06 22:58:39 +0800

    Initial Commit

---
+++ NotePlus/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  NotePlus
+//
+//  Created by 张俊 on 2020/1/6.
+//  Copyright © 2020 张俊. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
